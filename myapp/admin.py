cde04f8dc5f26d4e48aeb6b41c7581d7da77d750
specsxc <piotr.dolgoszyja1@gmail.com>
2025-01-29 02:30:28 +0100

    add admin panel

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post, Comment, Rate
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Rate)
